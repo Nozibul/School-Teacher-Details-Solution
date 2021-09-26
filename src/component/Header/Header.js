b2772a2d837ac68051dfa5d0745573362a6c5d9f
Nozibul <nozibulislamspi@gmail,com>
2021-09-26 08:57:11 +0600

    data

---
+++ src/component/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1> School Teachers Details</h1>
+            <p>Teachers, by far, have one of the most difficult jobs in the world,
+               They must deal with administrators and angry parents .
+            </p>
+            <h1>Total Salary per Month : $35000  </h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/component/data/Data.js
@@ -0,0 +1,98 @@
+const TeachersDetails = [
+    {
+        name: 'Doris Wilson',
+        qualifications:'BSc with QTS.',
+        subject: 'C',
+        age: 34,
+        address: 'Ness city',
+        salary : 25000
+    },
+    {
+        name: 'Amy Smith',
+        qualifications:'BSc in CSE',
+        subject: 'C++',
+        age: 44,
+        address: 'Baznie',
+        salary : 31000
+    },
+    {
+        name: ' Mrs. A. T. Whitecotton',
+        qualifications:'BSc in CSE',
+        subject: 'JAVA',
+        age: 32,
+        address: 'Aleander',
+        salary : 28000
+    },
+    {
+        name: 'Geraldine Carpenter',
+        qualifications:'MSc in CSE',
+        subject: 'JavaScript',
+        age: 38,
+        address: 'Rush Center',
+        salary : 45000
+    },
+    {
+        name: 'Edna Francis',
+        qualifications:'BSc with QTS.',
+        subject: 'Data Structure',
+        age: 25,
+        address: 'Gray',
+        salary : 18000
+    },
+    {
+        name: 'Jennie Crigler',
+        qualifications:'BSc in CSE',
+        subject: 'Algorithm',
+        age: 32,
+        address: 'Otis',
+        salary : 23000
+    },
+    {
+        name: 'Gladys Swon',
+        qualifications:'MSc in CSE.',
+        subject: 'Database',
+        age: 40,
+        address: 'Larned',
+        salary : 36000
+    },
+    {
+        name: 'Ruth Carman',
+        qualifications:'BSc in Mathemethics',
+        subject: 'Calculus',
+        age: 24,
+        address: 'Hudson',
+        salary : 19000
+    },
+    {
+        name: 'Lida Gray Gibbs',
+        qualifications:'BSc in CSE',
+        subject: 'Machine Learning',
+        age: 31,
+        address: 'Utica',
+        salary : 40000
+    },
+    {
+        name: 'Lela Pat Buckman',
+        qualifications:'BSc in CSE.',
+        subject: 'Syber Security',
+        age: 45,
+        address: 'Hanston',
+        salary : 25000
+    },
+    {
+        name: 'Abigail Marlow',
+        qualifications:'MSc in CSE',
+        subject: 'Networking',
+        age: 30,
+        address: 'Dundee',
+        salary : 26000
+    },
+    {
+        name: 'Irene Ball ',
+        qualifications:'BSc in CSE',
+        subject: 'Project Management',
+        age: 24,
+        address: 'Victoria',
+        salary : 27000
+    }
+]
